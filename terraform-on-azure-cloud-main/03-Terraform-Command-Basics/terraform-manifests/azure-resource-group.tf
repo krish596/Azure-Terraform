68b26988f6d1fb4867a241a93f964153c3e1c508
Chimtamreddy <kr7348202@gmail.com>
2025-07-02 16:17:51 +0530

    intial commit

--- terraform-on-azure-cloud-main/03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ terraform-on-azure-cloud-main/03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -1,23 +1,21 @@
+# Terraform Settings Block
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" # Optional but recommended in production
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
-
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+# Create Resource Group 
+resource "azurerm_resource_group" "my_demo_rg1" {
+  location = "eastus"
+  name = "my-demo-rg1"
 }
-
-
