fe5a5c58f3469707260019ac6b88585943f89ce6
Chimtamreddy <kr7348202@gmail.com>
2025-07-09 16:36:25 +0530

    intial commit

--- terraform-on-azure-cloud-main/05-Terraform-Provider-Resource-Block-Basics/sample-terraform-settings.tf
+++ terraform-on-azure-cloud-main/05-Terraform-Provider-Resource-Block-Basics/sample-terraform-settings.tf
@@ -1,17 +1,17 @@
 
 terraform {
-  # Required Terraform Version
-  required_version = ">= 1.0.0"  
-  # Required Providers and their Versions
-  required_providers {            
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.0" # Optional but recommended
-    }
-  }
+
   # Terraform State Storage to Azure Storage Container
   backend "azurerm" { 
-    resource_group_name   = "terraform-storage-rg"
+    resource_group_name   = "terraform-storage-rg"# Required Terraform Version
+    required_version = ">= 1.0.0"
+    # Required Providers and their Versions
+    required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = ">= 2.0" # Optional but recommended
+      }
+    }
     storage_account_name  = "terraformstate201"
     container_name        = "tfstatefiles"
     key                   = "terraform.tfstate"
