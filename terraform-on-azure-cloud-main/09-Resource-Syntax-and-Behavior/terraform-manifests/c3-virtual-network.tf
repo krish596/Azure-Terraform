3bfeb68e5c84fb845dca9eab8ca3440273cd8966
Chimtamreddy <kr7348202@gmail.com>
2025-07-11 22:48:43 +0530

    intial commit

--- terraform-on-azure-cloud-main/09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network.tf
+++ terraform-on-azure-cloud-main/09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network.tf
@@ -1,93 +1,47 @@
 
-# # Resource-2: Create Virtual Network
-# resource "azurerm_virtual_network" "myvnet" {
-#   name                = "myvnet-1"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.myrg.location
-#   resource_group_name = azurerm_resource_group.myrg.name
-#   tags = {
-#     "Name" = "myvnet-1"
-#     #"Environment" = "Dev"  # Uncomment during Step-10
-#   }
-# }
-#
-# # Resource-3: Create Subnet
-# resource "azurerm_subnet" "mysubnet" {
-#   name                 = "mysubnet-1"
-#   resource_group_name  = azurerm_resource_group.myrg.name
-#   virtual_network_name = azurerm_virtual_network.myvnet.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
-#
-# # Resource-4: Create Public IP Address
-# resource "azurerm_public_ip" "mypublicip" {
-#   name                = "mypublicip-1"
-#   resource_group_name = azurerm_resource_group.myrg.name
-#   location            = azurerm_resource_group.myrg.location
-#   allocation_method   = "Static"
-#   tags = {
-#     environment = "Dev"
-#   }
-# }
-#
-# # Resource-5: Create Network Interface
-# resource "azurerm_network_interface" "myvmnic" {
-#   name                = "vmnic"
-#   location            = azurerm_resource_group.myrg.location
-#   resource_group_name = azurerm_resource_group.myrg.name
-#
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.mysubnet.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id = azurerm_public_ip.mypublicip.id
-#   }
-# }
-#
-
-# # Resource-2: Create Virtual Network
-
-
-resource "azurerm_virtual_network" "myvm" {
-  name                = "myvm1"
+# Resource-2: Create Virtual Network
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "myvnet-1"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
-  address_space       = ["10.0.0.0/16"]
-
-
   tags = {
-    name = "myvm"
+    "Name" = "myvnet-1"
+    #"Environment" = "Dev"  # Uncomment during Step-10
+  }
+}
+
+# Resource-3: Create Subnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "mysubnet-1"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Resource-4: Create Public IP Address
+resource "azurerm_public_ip" "mypublicip" {
+  name                = "mypublicip-1"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  allocation_method   = "Static"
+  tags = {
     environment = "Dev"
   }
 }
-# # Resource-3: Create Subnet
-resource "azurerm_subnet" "myvnet" {
-  address_prefixes     = ["10.0.1.0/24"]
-  name                 = "myvnet"
-  resource_group_name  = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.myvm.name
-}
 
-
-# # Resource-4: Create Public IP Address
-resource "azurerm_public_ip" "mypublicip" {
-  allocation_method   = "Static"
-  location            = azurerm_resource_group.myrg.location
-  name                = "mypublicip"
-  resource_group_name = azurerm_resource_group.myrg.name
-}
-
-
-
-resource "azurerm_network_interface" "myvmnet" {
-  name                = "myvmnet-1"
+# Resource-5: Create Network Interface
+resource "azurerm_network_interface" "myvmnic" {
+  name                = "vmnic"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.myvnet.id
+    subnet_id                     = azurerm_subnet.mysubnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.mypublicip.id
   }
 }
+
+
