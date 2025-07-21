bfe9dc0a7a94dbd9c642347bf5efb624c8111305
Chimtamreddy <kr7348202@gmail.com>
2025-07-21 21:57:48 +0530

    intial commit

--- terraform-on-azure-cloud-main/11-Azure-Linux-Virtual-Machine/terraform-manifests/c7-04-web-linuxvm-network-security-group.tf
+++ terraform-on-azure-cloud-main/11-Azure-Linux-Virtual-Machine/terraform-manifests/c7-04-web-linuxvm-network-security-group.tf
@@ -1,4 +1,4 @@
-/*
+
 # Resource-3 (Optional): Create Network Security Group and Associate to Linux VM Network Interface
 # Resource-1: Create Network Security Group (NSG)
 resource "azurerm_network_security_group" "web_vmnic_nsg" {
@@ -38,7 +38,7 @@ resource "azurerm_network_security_rule" "web_vmnic_nsg_rule_inbound" {
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.web_vmnic_nsg.name
 }
-*/
+
 
 
 
